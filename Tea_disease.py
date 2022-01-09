97ad36304798b8e0bdd5983a7817197661feebd6
BrianBravoski <brianbravoski28@gmail.com>
2022-01-09 20:41:55 +0300

    added upload

--- Tea_disease.py
+++ Tea_disease.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
